a9e92437920bc91aa03ffb852b79b67aa5ff9753
uzb001 <137428777+uzb001@users.noreply.github.com>
2023-08-18 16:26:37 +0500

    Car1

---
+++ Car/Car/Cars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    internal class Cars
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public static decimal TaxPercentage { get; set; }
+        public Cars(int id, string model, string brand, decimal price)
+        {
+            Id = id;
+            Model = model;
+            Brand = brand;
+            Price = price;
+        }
+        public void CalculateTotal()
+        {
+            Price += Price * (TaxPercentage / 100);
+            Console.WriteLine($"Price with tax: {Price}, {TaxPercentage} %");
+        }
+        /*
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id}, Model: {Model}, Brand: {Brand}, Price: {Price}");
+        }
+        */
+    }
+}
---
+++ Car/Car/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars car = new Cars(001, "Audi", "Q7", 100_000);
+            Cars car1 = new Cars(002, "BYD", "XAN", 65_000);
+            Cars car2 = new Cars(003, "Chevrolet", "Malibu XL", 36_000);
+            
+            Cars.TaxPercentage = 50;
+
+            car.CalculateTotal();
+            car1.CalculateTotal();
+            car2.CalculateTotal();
+
+            Console.WriteLine("Another tax.");
+
+            Cars.TaxPercentage = 20;
+
+            car.CalculateTotal();
+            car1.CalculateTotal();
+            car2.CalculateTotal();
+        }
+    }
+}
